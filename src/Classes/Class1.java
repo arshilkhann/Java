fc6d595ed1d543197fa1af7f829c4fcdef94d706
ArshilKhan09 <arshilkhan529@gmail.com>
2026-08-06 20:04:20 +0530

    Added new class1.java for Understanding how class works

---
+++ src/Classes/Class1.java
@@ -0,0 +1,30 @@
+package Classes;
+
+public class Class1 {
+    public static void main(String[] args) {
+        Demo d1 = new Demo();
+        d1.name = "Rehan" ;
+        System.out.print("Object 1 : ");
+        d1.walk();
+
+        System.out.println();
+
+        Demo d2 = new Demo();
+        d2.name = "Farhan" ;
+        System.out.print("Object 2 : ");
+        d2.run();
+    }
+}
+
+
+
+class Demo {
+    String name ;
+    public static void walk(){
+        System.out.println("Walking");
+    }
+
+    public static void run(){
+        System.out.println("Running");
+    }
+}
