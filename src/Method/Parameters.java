9b4bf5a5d8677e117df5e247aeb3134464ac6702
arshilkhann <arshilkhan529@gmail.com>
2026-09-19 20:04:58 +0530

    added Parameter age

--- src/Method/Parameters.java
+++ src/Method/Parameters.java
@@ -3,15 +3,15 @@ package Method;
 //Parameters are the named variable passed in Method or Constructor that act as a placeholder for the data the object need to perform a task 
 public class Parameters {
 
-    static void myMethod(String name){ //here String name is parameter which is passed as a variable 
-        System.out.println("Hello! "+name);
+    static void myMethod(String name , int age){ //here String name is parameter which is passed as a variable 
+        System.out.println(name+"'s age is "+age);
     }
     public static void main(String[] args){
-        myMethod("Arshil");
-        myMethod("Rehan");
-        myMethod("Huzefa");
-        myMethod("Farhan");
-        myMethod("Talha");
-        myMethod("Javed");
+        myMethod("Arshil" , 20);
+        myMethod("Rehan",16);
+        myMethod("Huzefa",16);
+        myMethod("Farhan",20);
+        myMethod("Talha",12);
+        myMethod("Javed",17);
     }
 }
